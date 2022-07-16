bfc4179f8ba4295535e6a3d79b2e155cb5ac5b86
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-16 16:42:52 +0600

    4 Getting started with firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('hello form index.js')
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
+import { firebaseConfig } from './fireobj'
+
+// const firebaseConfig = require('./fireobj')
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA77osiT0_M_sdzPx2S2UBq-FNdWtnCtAE",
+//     authDomain: "firery-1.firebaseapp.com",
+//     projectId: "firery-1",
+//     storageBucket: "firery-1.appspot.com",
+//     messagingSenderId: "990920457191",
+//     appId: "1:990920457191:web:43c9aa5e55ce518a335b42"
+// };
+
+
+// init firbase app 
+initializeApp(firebaseConfig);
+
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+
+// get collection data 
+
+getDocs(colRef)
+    .then((res) => {
+        let books = []
+        res.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
+
+
